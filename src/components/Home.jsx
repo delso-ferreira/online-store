00355d2c7f41ccc95716fd83e6520cc0f4858cc3
delso-ferreira <delsofelipe@gmail.com>
2023-04-21 11:09:01 -0300

    wip req 06

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery,
+  getCategorieId } from '../services/api';
 
 class Home extends React.Component {
   state = {
     productsList: [],
     categoriesList: [],
+    search: '',
+    productFind: false,
   };
 
   componentDidMount() {
@@ -20,10 +23,50 @@ class Home extends React.Component {
     });
   };
 
+  querySearch = async () => {
+    const { search } = this.state;
+    const getSearch = await getProductsFromCategoryAndQuery(search);
+    console.log(getSearch);
+
+    if (getSearch) {
+      this.setState({
+        productsList: getSearch.results,
+        productFind: true,
+      });
+    } else {
+      this.setState({
+        productFind: false,
+      });
+    }
+  };
+
+  inputChange = (event) => {
+    const { target } = event;
+    const { value, name } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  showCheckedList = async () => {
+    // buscar o id da categoria ao clicar no radio button, o param tem que ser dinâmico
+    // a função consegue passar o paramêtro de forma hard coded [não dinâmica] atravês do id de um produto
+    // por exemplo, se colocarmos no ao inves do param, algo como 'MLB1055', conseguimos renderizar os celulares
+    // a minha ideia seria fazer um filter dentro do array, utilizando um includes da string referente ao radio button
+    // porém estou não consegui pensar em uma forma de fazer.
+    const productById = await getCategorieId(param);
+    console.log(productById);
+
+    this.setState({
+      productsList: productById.results,
+      productFind: true,
+    });
+  };
+
   render() {
-    const { productsList } = this.state;
+    const { productsList, categoriesList, search, productFind } = this.state;
     const empty = productsList.length === 0;
-    const { categoriesList } = this.state;
     return (
       <div>
         <Link data-testid="shopping-cart-button" to="/shopping-cart">
@@ -34,6 +77,23 @@ class Home extends React.Component {
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>
         )}
+
+        <input
+          data-testid="query-input"
+          type="text"
+          value={ search }
+          name="search"
+          onChange={ this.inputChange }
+        />
+
+        <button
+          data-testid="query-button"
+          type="button"
+          onClick={ this.querySearch }
+        >
+          Buscar
+        </button>
+
         {categoriesList.map((categoria) => (
           <div key={ categoria.id }>
             <label data-testid="category" htmlFor="categories">
@@ -42,10 +102,26 @@ class Home extends React.Component {
             <input
               type="radio"
               name="categories"
+              onClick={ this.showCheckedList }
               value={ categoria.name }
             />
           </div>
         ))}
+        {productFind
+          ? (productsList.map(({ price, thumbnail, title, id }) => (
+
+            <>
+              <div
+                key={ id }
+                data-testid="product"
+              />
+              <h1>{title}</h1>
+              <img src={ thumbnail } alt="thumbnail" />
+              <h2>{price}</h2>
+            </>
+
+          )))
+          : (<p>Nenhum produto foi encontrado</p>) }
 
       </div>
 
--- src/services/api.js
+++ src/services/api.js
@@ -14,3 +14,9 @@ export async function getProductById() {
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
 }
+
+export async function getCategorieId(categoryId) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+  const data = await response.json();
+  return data;
+}
